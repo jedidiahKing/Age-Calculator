323b248e2469c8bf519a30044324d162959d3f17
MELISSA <pc@MELISSA.com>
2021-04-18 19:16:55 +0000

    first commit

---
+++ assets/app.js
@@ -0,0 +1,22 @@
+function calculate() {
+	
+    var number = document.getElementById("number").value;
+    
+    if (number == "" || number == 0) {
+        
+        window.alert("Please type your correct age!");
+        return;
+    }
+    
+    var date = new Date().getFullYear();
+    
+    var birthyear = date - number;
+    
+    document.getElementById("year").style.display = "block";
+    document.getElementById("year").innerHTML = "Your birth year is " + birthyear;
+    
+}
+
+document.getElementById("year").style.display = "none";
+
+document.getElementById("button").onclick = function() {  calculate();  };
